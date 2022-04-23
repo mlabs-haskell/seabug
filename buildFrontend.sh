fa3dda99853ebeb02b521f98b40b4eb9b6ee1420
t4ccer <t4ccer@gmail.com>
2022-04-23 09:31:32 -0600

    Enable flakes in buildFrontend script

--- buildFrontend.sh
+++ buildFrontend.sh
@@ -1,1 +1,1 @@
-nix develop cardano-transaction-lib/ -L -c ./buildFrontendStage2.sh
+nix develop cardano-transaction-lib/ -L --extra-experimental-features "nix-command flakes" -c ./buildFrontendStage2.sh 
